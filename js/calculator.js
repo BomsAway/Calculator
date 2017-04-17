f89260760c2afa5f986a0a54c0b8334c3354823b
jerik0 <jcarrico1985@live.com>
2017-04-17 15:35:49 -0500

    added css and js folders, edited html

--- js/calculator.js
+++ js/calculator.js
@@ -2,6 +2,37 @@
 
 (function() {
 
+    var numberedButtons = document.getElementsByClassName('calc-button');
+    var operatorButtons = document.getElementsByClassName('operator');
+    var inputOne = document.getElementById('input-one');
+    var operator = document.getElementById('operand');
+    var inputTwo = document.getElementById('input-two');
+    var i=0;
+
+    function display() {
+        if (operator === '') {
+            inputOne.value = this.innerHTML;
+        } else {
+            inputTwo.value = this.innerHTML;
+        }
+        console.log(this.innerHTML);
+    }
+
+    for (i=0; i<numberedButtons.length; i++) {
+        numberedButtons[i].addEventListener("click", display);
+    }
+//-----------------------------------------
+    function operation() {
+        operator.value = this.innerHTML;
+    }
+
+    for (i=0; i<operatorButtons.length; i++) {
+        operatorButtons[i].addEventListener("click", operation);
+    }
+//-----------------------------------------
+
+
+
 
 
 })();
