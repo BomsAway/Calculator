065b360b062a9d5da18a1802218fe4f0f31ad804
Carlos <c.soto93@hotmail.com>
2017-04-18 09:52:36 -0500

    -calculator functionality

--- js/calculator.js
+++ js/calculator.js
@@ -4,6 +4,8 @@
 
     var numberedButtons = document.getElementsByClassName('calc-button');
     var operatorButtons = document.getElementsByClassName('operator');
+    var equals = document.getElementById('equals');
+    var clear = document.getElementById('clear');
     var inputOne = document.getElementById('input-one');
     var operator = document.getElementById('operand');
     var inputTwo = document.getElementById('input-two');
@@ -11,9 +13,9 @@
 
     function display() {
         if (operator.value === '') {
-            inputOne.value = this.innerHTML;
+            inputOne.value = inputOne.value + this.innerHTML;
         } else {
-            inputTwo.value = this.innerHTML;
+            inputTwo.value = inputTwo.value + this.innerHTML;
         }
         console.log(this.innerHTML);
     }
@@ -26,13 +28,54 @@
         operator.value = this.innerHTML;
     }
 
-    for (i=0; i<operatorButtons.length; i++) {
+    for ( i = 0; i < operatorButtons.length; i++) {
         operatorButtons[i].addEventListener("click", operation);
     }
 //-----------------------------------------
 
+    function total() {
 
+        if (inputTwo.value === '') {
+            alert('Invalid entry');
+        }
 
+        if (+inputOne.value / +inputTwo.value && +inputTwo.value === 0) {
+            inputOne.value = '';
+            inputTwo.value = '';
+            operator.value = '';
+            alert("You can't do that... THAT'S IMPOSSIBLE!")
+        }
 
+        switch (operator.value) {
+            case "+":
+                inputOne.value = +inputOne.value + +inputTwo.value;
+                inputTwo.value = '';
+                break;
+            case "-":
+                inputOne.value = +inputOne.value - +inputTwo.value;
+                inputTwo.value = '';
+                break;
+            case "X":
+                inputOne.value = +inputOne.value * +inputTwo.value;
+                inputTwo.value = '';
+                break;
+            case "/":
+                inputOne.value = +inputOne.value / +inputTwo.value;
+                inputTwo.value = '';
+                break;
+
+        }
+    }
+
+    equals.addEventListener("click", total);
+//-------------------------------------------
+
+    function clearing() {
+        inputOne.value = '';
+        inputTwo.value = '';
+        operator.value = '';
+    }
+
+    clear.addEventListener("click", clearing);
 
 })();
